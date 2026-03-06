4dd95b9486c80b291129552ba6b495c6b15893c6
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-06 14:04:10 +1100

    Update display.js

--- display.js
+++ display.js
@@ -1,10 +1,12 @@
 fetch("data.json")
 /*.then(res => res.json())
-.then(data => {*/
+.then(data => {
 
 document.querySelector("#uploads").textContent = data.stats.uploads;
 document.querySelector("#creators").textContent = data.stats.creators;
 document.querySelector("#favorites").textContent = data.stats.favorites;
-document.querySelector("#members").textContent = data.stats.members;
+document.querySelector("#members").textContent = data.stats.members;*/
+
+document.getElementById("uploads").textContent = data.stats.uploads;
 
 });
