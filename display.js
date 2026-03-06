6111281fa2ed5a83b28602028d15976d5ef32517
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-06 15:01:24 +1100

    Update display.js

--- display.js
+++ display.js
@@ -1,8 +1,11 @@
-fetch("test.json")
+fetch("data.json")
 .then(response => response.json())
 .then(data => {
 
 document.getElementById("uploads").textContent = data.stats.uploads;
+document.getElementById("creators").textContent = data.stats.creators;
+document.getElementById("favorites").textContent = data.stats.favorites;
+document.getElementById("members").textContent = data.stats.members;
 })
 .catch(error => console.error('Error fetching data:', error));
 
