45ab74fc6592c2f5a938e6de4e9443ad743596e2
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-06 13:38:26 +1100

    Create display.js

---
+++ display.js
@@ -0,0 +1,10 @@
+fetch("data.json")
+.then(res => res.json())
+.then(data => {
+
+document.querySelector("#uploads").textContent = data.stats.uploads;
+document.querySelector("#creators").textContent = data.stats.creators;
+document.querySelector("#favorites").textContent = data.stats.favorites;
+document.querySelector("#members").textContent = data.stats.members;
+
+});
