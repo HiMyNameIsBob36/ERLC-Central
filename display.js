ac9267f3b94a7e7be71b3ec6cb4478f31be29a30
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-03-06 14:51:59 +1100

    Update display.js

--- display.js
+++ display.js
@@ -1,2 +1,8 @@
 fetch("data.json")
+.then(response => response.json())
+.then(data => {
+
 document.getElementById("uploads").textContent = data.stats.uploads;
+})
+.catch(error => console.error('Error fetching data:', error));
+
